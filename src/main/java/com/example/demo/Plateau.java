ef04f5aa464ff44207dd8f384b2a91dd5d351b0a
jenni <jenniferharries71@gmail.com>
2025-05-27 17:08:49 +0200

    jai travaille bande de chiens

---
+++ src/main/java/com/example/demo/Plateau.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class Plateau {
+    private int totalCase;
+    private int totalPlayers;
+    private String caca;
+}
---
+++ src/main/java/com/example/demo/Player.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class Player {
+    private String name;
+    private String color;
+    private int numCase;
+    private int state;
+    private int numPlayer;
+
+    public Player(String name, String color, int numCase, int numPlayer){
+
+    }
+
+    public void setColor(String color) {this.color = color;}
+
+    public void setName(String name) {this.name = name;}
+
+    public void setState(int state) {this.state = state;}
+
+    public void setNumCase(int add) {this.numCase += add;}
+
+    public String getName() {return name;}
+
+    public int getNumCase() {return numCase;}
+
+    public int getNumPlayer() {return numPlayer;}
+
+    public int diceThrow(){
+        if (state==0){
+            ;
+        }
+        if (this.state==1){
+            Player.setNumCase(number.nextInt(1,6));
+        }
+    }
+
+    public void restartGame(){
+        this.state=1;
+        this.numCase=1;
+    }
+}
