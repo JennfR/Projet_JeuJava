693d06623143dba74e06cc7b4bee97f09c5a56ac
Justin Ostorero <justin.ostorero@hes-so.ch>
2025-06-02 17:29:35 +0200

    Création de la classe Card

---
+++ src/main/java/Timeline/Card.java
@@ -0,0 +1,18 @@
+package Timeline;
+
+public class Card {
+   private String name;
+   private int date;
+   private int number;
+   private String imagePath;
+   public Card(String name, int date, String imagePath )
+   {
+       this.name = name;
+       this.date = date;
+       this.imagePath= imagePath;
+   }
+   public void place_card (Card placed_card)
+   {
+
+   }
+}
