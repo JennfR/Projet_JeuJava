c39412e18eaa56fc4c0a3106fd8d7c8571cc0323
gabriel paulat <gab74350@gmail.com>
2025-06-03 22:15:57 +0200

    debut code controller HomePage

--- src/main/java/HomePage/HomePageController.java
+++ src/main/java/HomePage/HomePageController.java
@@ -1,31 +1,54 @@
 package HomePage;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import java.io.IOException;
 
 public class HomePageController {
-    HomePageController homePageController = new HomePageController();
+
+    private Stage stage = new Stage();
 
     @FXML
     public void startSnake(){
         System.out.println("Snake");
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePageApplication.class.getResource("Timeline.fxml"));
+        try {
+            // Load the scene from FXML and show it in a new stage
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.setTitle("Planet around the Sun");
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
     }
 
     @FXML
     public void startTimeline() {
-
         System.out.println("TimeLine");
+        FXMLLoader fxmlLoader = new FXMLLoader(HomePageApplication.class.getResource("Snake.fxml"));
+        try {
+            // Load the scene from FXML and show it in a new stage
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setScene(scene);
+            stage.setTitle("Planet around the Sun");
+            stage.show();
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
+        }
     }
 
     @FXML
     public void startJeuDuJour(){
         int day = 0;
         if (day%2==0){
-            homePageController.startSnake();
+            this.startSnake();
         }else{
-            homePageController.startTimeline();
+            this.startTimeline();
         }
     }
 
